b770b30499c9570502dc80ac4ff9b304149b6a76
Oleksandr Podze <dasdeg@gmail.com>
2023-12-03 00:09:13 +0100

    add 2023 d01, d02

---
+++ 2023/d02/d02.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+func linesFromFile(p string) ([]string, error) {
+	log.Println(os.Getwd())
+	file, err := os.Open(p)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	res := make([]string, 0)
+	for scanner.Scan() {
+		res = append(res, scanner.Text())
+	}
+	return res, nil
+}
+
+func oneLine(line string) int {
+	re := regexp.MustCompile(`Game (\d+): (.+)`)
+	matches := re.FindStringSubmatch(line)
+	rest := matches[2]
+	subGames := strings.Split(rest, ";")
+
+	colorsSeen := make(map[string]int, 0)
+	for _, part := range subGames {
+		parts := strings.Split(strings.TrimSpace(part), ",")
+		for _, p := range parts {
+			p = strings.TrimSpace(p)
+			var amtStr, color string
+			fmt.Sscanf(p, "%s %s", &amtStr, &color)
+
+			amt, _ := strconv.Atoi(amtStr)
+
+			amtCur := colorsSeen[color]
+			if amt > amtCur {
+				colorsSeen[color] = amt
+			}
+		}
+	}
+	// Part 1
+	// if colorsSeen["red"] <= 12 && colorsSeen["green"] <= 13 && colorsSeen["blue"] <= 14 {
+	// gameIndex, _ := strconv.Atoi(matches[1])
+	// 	return gameIndex
+	// }
+	//
+	// return 0
+	// Part 2
+	return colorsSeen["red"] * colorsSeen["green"] * colorsSeen["blue"]
+}
+
+func sumAllLines(lines []string) int {
+	res := 0
+
+	for _, line := range lines {
+		res += oneLine(line)
+	}
+	return res
+}
+
+func main() {
+	lines, err := (linesFromFile("./input"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(len(lines))
+	fmt.Println(oneLine("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"))
+	fmt.Println(oneLine("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue"))
+	fmt.Println(oneLine("Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red"))
+	fmt.Println(oneLine("Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red"))
+	fmt.Println(oneLine("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"))
+	fmt.Println("Part 1 is: ", sumAllLines(lines))
+}
