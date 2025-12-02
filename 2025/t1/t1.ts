04e067a255084d82a5fc868071531a176d96f11e
Oleksandr Podze <dasdeg@gmail.com>
2025-12-02 10:08:52 +0100

    format 2025-day1

--- 2025/t1/t1.ts
+++ 2025/t1/t1.ts
@@ -1,54 +1,54 @@
-import * as fs from 'fs';
+import * as fs from "fs";
 
-const lines = fs.readFileSync('inp_base.txt', 'utf-8').trim().split('\n');
-const lines2 = fs.readFileSync('inp_full.txt', 'utf-8').trim().split('\n');
+const lines = fs.readFileSync("inp_base.txt", "utf-8").trim().split("\n");
+const lines2 = fs.readFileSync("inp_full.txt", "utf-8").trim().split("\n");
 
 function part1(lines: string[]): void {
-    let position = 50;
-    let zeros = 0;
+  let position = 50;
+  let zeros = 0;
 
-    for (const l of lines) {
-        let mult = (l[0] === 'L') ? -1 : 1;
-        const leftover = parseInt(l.slice(1)) % 100;
-        position += mult * leftover;
+  for (const l of lines) {
+    let mult = l[0] === "L" ? -1 : 1;
+    const leftover = parseInt(l.slice(1)) % 100;
+    position += mult * leftover;
 
-        if (position < 0) {
-            position = 100 + position;
-        } else {
-            position = position % 100;
-        }
-
-        if (position === 0) {
-            zeros++;
-        }
+    if (position < 0) {
+      position = 100 + position;
+    } else {
+      position = position % 100;
     }
 
-    console.log(zeros);
+    if (position === 0) {
+      zeros++;
+    }
+  }
+
+  console.log(zeros);
 }
 
 function part2(lines: string[]): void {
-    let position = 50;
-    let zeros = 0;
+  let position = 50;
+  let zeros = 0;
 
-    for (const l of lines) {
-        let mult = (l[0] === 'L') ? -1 : 1;
-        const prev = position;
-        const clicks = parseInt(l.slice(1));
-        zeros += Math.floor(clicks / 100);
-        const leftover = clicks % 100;
-        position += mult * leftover;
+  for (const l of lines) {
+    let mult = l[0] === "L" ? -1 : 1;
+    const prev = position;
+    const clicks = parseInt(l.slice(1));
+    zeros += Math.floor(clicks / 100);
+    const leftover = clicks % 100;
+    position += mult * leftover;
 
-        if (prev !== 0 && (position <= 0 || position > 99)) {
-            zeros++;
-        }
-        if (position < 0) {
-            position = 100 + position;
-        } else {
-            position = position % 100;
-        }
+    if (prev !== 0 && (position <= 0 || position > 99)) {
+      zeros++;
     }
+    if (position < 0) {
+      position = 100 + position;
+    } else {
+      position = position % 100;
+    }
+  }
 
-    console.log(zeros);
+  console.log(zeros);
 }
 
 part1(lines);
